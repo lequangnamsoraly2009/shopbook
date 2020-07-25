e31e4bbc0da678490e3928074a6f152204a5df9b
lequangnamsoraly2009 <lequangnam1617@gmail.com>
2020-07-25 21:57:15 +0700

    update list

--- app.js
+++ app.js
@@ -1,20 +1,180 @@
 const express = require('express')
 var createError = require('http-errors');
 var path = require('path');
+const bodyParser = require('body-parser')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [], singers: [], songs: [] })
+    .write()
 
 var app = express();
 
 var port = 3000
 
 // Set 
-
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+// Use
 app.use(express.static(__dirname + '/public'));
-//
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+//Get
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.get('/login', (req, res) => {
+    res.render('login/sign-in')
+});
+
+app.get('/logout', (req, res) => {
+    res.render('logout/sign-up')
+});
+
+// list song vietnam
+
+app.get('/list-song/vn', (req, res) => {
+    res.render('listsong/vn/listsong', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/vn/young-music', (req, res) => {
+    res.render('listsong/vn/young-music', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/vn/lyrical-music', (req, res) => {
+    res.render('listsong/vn/lyrical-music', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/vn/hometown', (req, res) => {
+    res.render('listsong/vn/hometown-music', {
+        songs: db.get('songs').value(),
+    })
+});
+app.get('/list-song/vn/vn-rap', (req, res) => {
+    res.render('listsong/vn/vn-rap', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/vn/vn-rock', (req, res) => {
+    res.render('listsong/vn/vn-rock', {
+        songs: db.get('songs').value(),
+    })
+});
+
+
+app.get('/list-song/vn/:id', (req, res) => {
+    var idSong = parseInt(req.params.id);
+    res.render('songs/song', {
+        song: db.get('songs').find({ id: idSong }).value(),
+    })
+});
+
+// list song american
+app.get('/list-song/american', (req, res) => {
+    res.render('listsong/american/listsong', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/american/country', (req, res) => {
+    res.render('listsong/american/country', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/american/electronic', (req, res) => {
+    res.render('listsong/american/electronic', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/american/pop', (req, res) => {
+    res.render('listsong/american/pop', {
+        songs: db.get('songs').value(),
+    })
+});
+app.get('/list-song/american/rock', (req, res) => {
+    res.render('listsong/american/rock', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/american/r&b', (req, res) => {
+    res.render('listsong/american/R&B', {
+        songs: db.get('songs').value(),
+    })
+});
+
+
+app.get('/list-song/american/:id', (req, res) => {
+    var idSong = parseInt(req.params.id);
+    res.render('songs/song', {
+        song: db.get('songs').find({ id: idSong }).value(),
+    })
+});
+
+// list-song asia
+app.get('/list-song/asia', (req, res) => {
+    res.render('listsong/asia/listsong', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/asia/china', (req, res) => {
+    res.render('listsong/asia/china', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/asia/india', (req, res) => {
+    res.render('listsong/asia/india', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/asia/japan', (req, res) => {
+    res.render('listsong/asia/japan', {
+        songs: db.get('songs').value(),
+    })
+});
+app.get('/list-song/asia/korean', (req, res) => {
+    res.render('listsong/asia/korean', {
+        songs: db.get('songs').value(),
+    })
+});
+
+app.get('/list-song/asia/:id', (req, res) => {
+    var idSong = parseInt(req.params.id);
+    res.render('songs/song', {
+        song: db.get('songs').find({ id: idSong }).value(),
+    })
+});
+
+app.get('/singers/:id', (req, res) => {
+    var idSinger = parseInt(req.params.id);
+    res.render('singers/singer', {
+        singer: db.get('singers').find({ id: idSinger }).value()
+    })
+});
+
+
+// Post
 
 app.listen(port, () => { console.log('App listening on port ' + port) });
---
+++ public/javascripts/validate.js
@@ -0,0 +1,126 @@
+function Validator(options) {
+
+    // Function tìm kiếm element cha có class cần tìm
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement
+            }
+            element = element.parentElement
+        }
+    }
+    // Object chứa các rules
+    var objectRules = {}
+
+    // Hàm thực hiện validate
+    function validate(inputElement, x) {
+        var errorMessage;
+        var errorElement = getParent(inputElement, options.formInputGroup).querySelector(options.errorSelector)
+            // Duyệt qua các rules trong mảng của input được select
+        for (var i = 0; i < objectRules[x.selector].length; i++) {
+            errorMessage = objectRules[x.selector][i](inputElement.value)
+            if (errorMessage) break
+        }
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage
+            getParent(inputElement, options.formInputGroup).classList.add('invalid')
+        } else {
+            errorElement.innerText = ''
+            getParent(inputElement, options.formInputGroup).classList.remove('invalid')
+        }
+        return !errorMessage
+    }
+
+    // Get Element của Form để xử Lý Validate
+    var formElement = document.querySelector(options.form)
+
+    if (formElement) {
+
+        // Xử lý các rules khi nhấn submit
+        formElement.onsubmit = e => {
+            e.preventDefault();
+            // Khởi tạo biến kiểm tra form có nhập lỗi ?
+            var formValid = true;
+            options.rules.forEach(x => {
+                var inputElement = formElement.querySelector(x.selector)
+                var isValid = validate(inputElement, x)
+                if (!isValid) {
+                    formValid = false;
+                }
+            })
+            if (formValid) {
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = formElement.querySelectorAll('[name]')
+                    var formValues = Array.from(enableInputs).reduce((values, input) => {
+                        values[input.name] = input.value
+                        return values
+                    }, {})
+                    options.onSubmit(formValues)
+                } else {
+                    formElement.submit();
+                }
+            }
+        }
+
+        options.rules.forEach((x) => {
+            // Nếu 1 input có nhiều rules thì push những rules của input đó vào 1 mảng của input đó
+            if (Array.isArray(objectRules[x.selector])) {
+                // Nếu input đã có 1 rule trong object thì push rule tiếp theo vào mảng của input đó trong obj
+                objectRules[x.selector].push(x.test)
+            } else {
+                // Nếu input chưa có rule thì cho rule mà object
+                objectRules[x.selector] = [x.test]
+            }
+
+            var inputElement = formElement.querySelector(x.selector)
+            if (inputElement) {
+                // Xử lý onblur khi nhấp ra khỏi thẻ input
+                inputElement.onblur = () => {
+                        validate(inputElement, x)
+                    }
+                    // Xử lý onblur mất đi khi bắt đầu nhập lại vào thẻ input
+                inputElement.oninput = () => {
+                    var errorElement = getParent(inputElement, options.formInputGroup).querySelector(options.errorSelector)
+                    errorElement.innerText = '';
+                    getParent(inputElement, options.formInputGroup).classList.remove('invalid')
+                }
+            }
+        })
+    }
+}
+
+
+// Rules 
+Validator.isRequired = (selector, message) => {
+    return {
+        selector: selector,
+        test: value => {
+            return value.trim() ? undefined : 'Please enter this field'
+        }
+    };
+}
+
+Validator.isEmail = (selector, message) => {
+    return {
+        selector: selector,
+        test: value => {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            return regex.test(value) ? undefined : value.length === 0 ? 'Please enter this field' : message
+        }
+    };
+}
+
+Validator.minLength = (selector, min, message) => {
+    return {
+        selector: selector,
+        test: value => value.length >= min ? undefined : value.length > 0 ? `Minimum password length is ${min} and includes number` : message
+    };
+}
+
+Validator.isMatched = (selector, confirmPass, message) => {
+    return {
+        selector: selector,
+        test: value => value === confirmPass() ? undefined : value.length === 0 ? 'Please enter this field' : message
+    }
+}
